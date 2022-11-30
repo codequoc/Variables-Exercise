066cccd9a37e23d4c6db57a84176b8c592a10fa8
Henry Nguyen <nguyen.henry796@gmail.com>
2022-11-30 00:31:12 -0500

    completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Rachel";
+            int age = 26;
+            char lastInitial = 'J';
+            bool isFemale = true;
+            double loans = 400.65;
+            decimal height = 5.9m;
+            Console.WriteLine($"My friend's name is {name}, and she is {age} years old. Her last initial is {lastInitial}, and is {height} feet tall. She is a female which is {isFemale}. She owns some student debt and has ${loans} left.");
         }
     }
 }
